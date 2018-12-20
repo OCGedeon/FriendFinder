9c6d11000a36a3d8b2d7c0a501b6cc2f2215767c
OmarCharlesGedeon <charles@effortlesshost.com>
2018-12-20 11:47:15 -0800

    Completed apiRoutes.js.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,16 @@ var friendsData = require ('../data/friends.js');
 
 module.exports = function (app) {
 
-    app.get ("/api/friends.js", function(req, res) {
-
-
-        
-    }) 
-
-}
+        app.get('/api/friends', function(req, res) {
+            res.json(friendsData);
+        });
+    
+    
+        app.post('/api/friends', function(req, res){
+            // if(friendsData.length<5) {
+                friendsData.push(req.body);
+                res.json(true);
+            // }
+        });
+    
+    }
