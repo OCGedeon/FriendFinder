a3d7b6bb160bfaedfaba0d36297d15dd0036e5da
OmarCharlesGedeon <charles@effortlesshost.com>
2018-12-19 21:35:32 -0800

    moved folders around.

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ var app = express();
 // Sets an initial port. Prevents having to reconfigure when deployed
 var PORT = process.env.PORT || 8080;
 
+// BodyParser makes it possible for our server to interpret data sent to it.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 // Listen
 
